e85140755dbc75ca279226f734ed906efdc8d5a0
PostScription <68855126+PostScipton@users.noreply.github.com>
2021-04-22 15:35:48 +0300

    New or modified exceptions

--- src/Exceptions/ShouldPublishConfigFileException.php
+++ src/Exceptions/ShouldPublishConfigFileException.php
@@ -2,16 +2,15 @@
 
 namespace PostScripton\Money\Exceptions;
 
-use Exception;
 
-class ShouldPublishConfigFileException extends Exception
+class ShouldPublishConfigFileException extends BaseException
 {
-	/**
-	 * @param int $code
-	 * @param Exception|null $previous
-	 */
-	public function __construct($code = 0, Exception $previous = null)
-	{
-		parent::__construct('Please publish the config file by running \'php artisan vendor:publish --tag=money\'', $code, $previous);
-	}
+    public function __construct($code = 0, BaseException $previous = null)
+    {
+        parent::__construct(
+            'Please publish the config file by running \'php artisan vendor:publish --tag=money\'',
+            $code,
+            $previous
+        );
+    }
 }
