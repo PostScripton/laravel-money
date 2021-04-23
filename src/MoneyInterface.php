44d28bf8ed35fc63dcbed52996bbd4d4941c3d37
PostScription <68855126+PostScipton@users.noreply.github.com>
2021-04-23 18:34:26 +0300

    Write docs for methods of Money and MoneySettings

--- src/MoneyInterface.php
+++ src/MoneyInterface.php
@@ -6,76 +6,142 @@ interface MoneyInterface
 {
     // ========== STATIC ========== //
 
-    /** Устанавливает значения
+    /**
+     * Sets default settings for any Money object
      * @param MoneySettings $setting
      */
     public static function set(MoneySettings $setting): void;
 
-    /** Возвращает чистое денежное число <p>
-     * К примеру, "1 234.56 ₽" -> "1234.56" </p>
+    /**
+     * Returns just a formatted number <p>
+     * For example, "$ 1 234.56" -> "1234.56" </p>
      * @param Money $money <p>
-     * Денежная строка: "1 234.56 ₽" </p>
+     * Money object: "1 234.56 ₽" </p>
      * @return string <p>
-     * Чистое денежное число: "1234.56" </p>
+     * Formatted number: "1 234.56" </p>
      */
     public static function purify(Money $money): string;
 
-    /** Возвращает целое денежное число из десятичного
+    /**
+     * Returns converted the money's value into integer for storing in database
      * @param Money $money <p>
-     * Денежное число: 1234.567 (установлено 2 дес. знака) </p>
+     * Money object: 1234.567 (set 2 decimals) </p>
      * @return int <p>
-     * Чистое целое денежное число: 123456 </p>
+     * Integer: 123456 </p>
      */
     public static function integer(Money $money): int;
 
     /**
-     * Конвертирует денежную строку из одной валюты в другую
+     * Converts from one currency to another
      * @param Money $money <p>
-     * Денежная строка, полученная методом format()
-     * </p>
+     * Money object </p>
      * @param Currency $into <p>
-     * Получившаяся фалюта (константа)
-     * </p>
+     * Currency to be converted into </p>
      * @param float $coeff
-     * <p>Разница между конвертируемой валютой и получившейся.</p>
+     * <p>Coefficient between one currency and another</p>
      * <p>USD -> RUB = 75.79 / 1</p>
      * <p>RUB -> USD = 1 / 75.79</p>
      * @return Money Денежная строка со знаком валюты
      */
     public static function convertOffline(Money $money, Currency $into, float $coeff): Money;
 
-    /** Корректирует ввод &lt;input type="number" /&gt; под правильные значения
+    /**
+     * Corrects input &lt;input type="number" /&gt; using default settings
      * @param string $input <p>
-     * Строка, полученная из input: "1234.567890" </p>
+     * Input string: "1234.567890" </p>
      * @return string <p>
-     * Откорректированная строка: "1234.5" </p>
+     * Corrected string: "1234.5" </p>
      */
     public static function correctInput(string $input): string;
 
-    // todo отдельным коммитом дописать переводы на английском
+    /**
+     * Returns the default divisor
+     * @return int
+     */
     public static function getDefaultDivisor(): int;
 
+    /**
+     * Returns the default number of decimals
+     * @return int
+     */
     public static function getDefaultDecimals(): int;
 
+    /**
+     * Returns the default thousand separator
+     * @return string
+     */
     public static function getDefaultThousandsSeparator(): string;
 
+    /**
+     * Returns the default decimal separator
+     * @return string
+     */
     public static function getDefaultDecimalSeparator(): string;
 
+    /**
+     * Returns whether money ends with 0 or not
+     * @return bool
+     */
     public static function getDefaultEndsWith0(): bool;
 
+    /**
+     * Returns whether there is a space between currency sign and number
+     * @return bool
+     */
     public static function getDefaultSpaceBetween(): bool;
 
+    /**
+     * Returns the default currency
+     * @return Currency
+     */
     public static function getDefaultCurrency(): Currency;
+
+    /**
+     * Returns the default origin. Whether it is integer or float
+     * @return int
+     */
+    public static function getDefaultOrigin(): int;
 
     // ========== OBJECT ========== //
 
+    /**
+     * Returns a formatted number <p>
+     * For example, "$ 1 234.5" -> "1 234.5" </p>
+     * @return string
+     */
     public function getNumber(): string;
 
+    /**
+     * Returns a pure number that uses for calculations. Not usually used <p>
+     * For example, you see "13.3" but within it looks like 13.276686139139672 </p>
+     * @return float
+     */
     public function getPureNumber(): float;
 
+    /**
+     * Converts money into another currency using coefficient between currencies
+     * <p>USD -> RUB = 75.79 / 1</p>
+     * <p>RUB -> USD = 1 / 75.79</p>
+     * @param Currency $currency <p>
+     * Currency you want to convert into </p>
+     * @param float $coeff <p>
+     * Coefficient between the money's currency and the chosen one
+     * @return Money
+     */
     public function convertOfflineInto(Currency $currency, float $coeff): Money;
 
+    /**
+     * Converts the money into integer for storing in database <p>
+     * For example, "1 234.5" -> 12345 </p>
+     * @return int
+     */
     public function toInteger(): int;
 
+    /**
+     * Returns the money string applying all the settings <p>
+     * You may not use it if you explicitly assign the object to a string </p>
+     * @return string <p>
+     * "$ 1 234.5" </p>
+     */
     public function toString(): string;
 }
--- src/MoneySettingsInterface.php
+++ src/MoneySettingsInterface.php
@@ -4,38 +4,101 @@ namespace PostScripton\Money;
 
 interface MoneySettingsInterface
 {
+    /**
+     * Sets decimals <p>
+     * 123.(4) </p>
+     * @param int $decimals
+     * @return MoneySettings
+     */
     public function setDecimals(int $decimals = 1): MoneySettings;
 
+    /**
+     * Sets a thousand separator <p>
+     * 1( )000( )000 </p>
+     * @param string $separator
+     * @return MoneySettings
+     */
     public function setThousandsSeparator(string $separator): MoneySettings;
 
+    /**
+     * Sets a decimal separator <p>
+     * 123(.)4</p>
+     * @param string $separator
+     * @return MoneySettings
+     */
     public function setDecimalSeparator(string $separator): MoneySettings;
 
+    /**
+     * Sets whether a number ends with 0 or not
+     * <p>true: 100(.0) </p>
+     * <p>false: 100() </p>
+     * @param bool $ends
+     * @return MoneySettings
+     */
     public function setEndsWith0(bool $ends = false): MoneySettings;
 
-    /** Есть ли пробел между знаком валюты и числом
+    /**
+     * Sets whether there is a space between currency sign and number
+     * <p>true: "$ 123.4" </p>
+     * <p>false: "$123.4" </p>
      * @param bool $space
      * @return MoneySettings
      */
     public function setHasSpaceBetween(bool $space = true): MoneySettings;
 
+    /**
+     * Sets a currency
+     * @param Currency $currency
+     * @return MoneySettings
+     */
     public function setCurrency(Currency $currency): MoneySettings;
 
+    /**
+     * Sets origin of number data from database, whether it is stored there as integer or float <p>
+     * For supported values use MoneySettings::ORIGIN_* </p>
+     * @param int $origin
+     * @return MoneySettings
+     */
     public function setOrigin(int $origin): MoneySettings;
 
+    /**
+     * Returns the decimals
+     * @return int
+     */
     public function getDecimals(): int;
 
+    /**
+     * Returns the thousand separator
+     * @return string
+     */
     public function getThousandsSeparator(): string;
 
+    /**
+     * Returns the decimal separator
+     * @return string
+     */
     public function getDecimalSeparator(): string;
 
+    /**
+     * Returns whether the money ends with 0 or not
+     * @return bool
+     */
     public function endsWith0(): bool;
 
-    /** Есть ли пробел между знаком валюты и числом
+    /** Returns whether there is a space between currency and number or not
      * @return bool
      */
     public function hasSpaceBetween(): bool;
 
+    /**
+     * Returns the currency
+     * @return Currency
+     */
     public function getCurrency(): Currency;
 
+    /**
+     * Returns the origin
+     * @return int
+     */
     public function getOrigin(): int;
 }
