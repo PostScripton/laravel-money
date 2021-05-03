b1708209ded05f27304307c58824c9e251fbcca7
PostScription <68855126+PostScipton@users.noreply.github.com>
2021-05-03 22:42:23 +0300

    Display and list settings, code supports both iso and number, more info

--- src/Currency.php
+++ src/Currency.php
@@ -2,7 +2,11 @@
 
 namespace PostScripton\Money;
 
+use Illuminate\Support\Collection;
 use PostScripton\Money\Exceptions\CurrencyDoesNotExistException;
+use PostScripton\Money\Exceptions\CurrencyHasWrongConstructorException;
+use PostScripton\Money\Exceptions\CurrencyListConfigException;
+use PostScripton\Money\Exceptions\NoSuchCurrencySymbolException;
 use PostScripton\Money\Exceptions\ShouldPublishConfigFileException;
 
 class Currency
@@ -12,38 +16,76 @@ class Currency
     public const POS_START = 0;
     public const POS_END = 1;
 
+    public const DISPLAY_SYMBOL = 10;
+    public const DISPLAY_CODE = 11;
+
+    public const LIST_ALL = 'all';
+    public const LIST_POPULAR = 'popular';
+    private static string $_list;
+
     public static function code(string $code): ?Currency
     {
-        $code = strtoupper($code);
+        $currency = is_numeric($code)
+            ? self::currencies()->firstWhere('num_code', $code)
+            : self::currencies()->firstWhere('iso_code', strtoupper($code));
 
-        if (!array_key_exists($code, self::all())) {
-            throw new CurrencyDoesNotExistException(__METHOD__, 1, '$code', $code);
+        if (is_null($currency)) {
+            throw new CurrencyDoesNotExistException(__METHOD__, 1, '$code', implode(',', [$code, self::$_list]));
         }
 
-        $currency = self::all()[$code];
-
-        return new Currency($code, $currency['symbol'], $currency['position']);
+        return new Currency($currency);
     }
 
-    protected static function all(): array
+    protected static function currencies(): Collection
     {
-        if (!static::$currencies) {
-            static::$currencies = require __DIR__ . '/List/currencies.php';
+        if (!in_array(config('money.currency_list'), [self::LIST_ALL, self::LIST_POPULAR])) {
+            throw new CurrencyListConfigException(config('money.currency_list'));
         }
-        return static::$currencies;
+
+        if (!self::$currencies) {
+            self::setCurrencyList(config('money.currency_list'));
+        }
+
+        return collect(self::$currencies);
+    }
+
+    public static function setCurrencyList(string $list = self::LIST_POPULAR)
+    {
+        if ($list !== self::LIST_ALL && $list !== self::LIST_POPULAR) {
+            $list = self::LIST_POPULAR;
+        }
+
+        self::$currencies = require __DIR__ . "/List/{$list}_currencies.php";
+        self::$_list = $list;
     }
 
     // ==================== OBJECT ==================== //
 
-    private string $code;
-    private string $symbol;
-    private string $position;
+    private string $full_name;
+    private string $name;
+    private string $iso_code;
+    private string $num_code;
+    private $symbol; // array or string
+    private int $position;
+    private int $display;
 
-    public function __construct(string $code, string $symbol, ?int $position = null)
+    public function __construct(array $currency)
     {
-        $this->code = $code;
-        $this->symbol = $symbol;
-        $this->position = $position ?? self::POS_START;
+        if (is_null($currency['full_name']) ||
+            is_null($currency['name']) ||
+            is_null($currency['iso_code']) ||
+            is_null($currency['num_code']) ||
+            is_null($currency['symbol'])) {
+            throw new CurrencyHasWrongConstructorException();
+        }
+
+        $this->full_name = $currency['full_name'];
+        $this->name = $currency['name'];
+        $this->iso_code = $currency['iso_code'];
+        $this->num_code = $currency['num_code'];
+        $this->symbol = $currency['symbol'];
+        $this->position = $currency['position'] ?? self::POS_END;
+        $this->display = self::DISPLAY_SYMBOL;
     }
 
     /**
@@ -58,13 +100,45 @@ class Currency
         return config('money.default_currency', 'USD');
     }
 
-    public function getCode(): string
+    public function getFullName(): string
     {
-        return $this->code;
+        return $this->full_name;
     }
 
-    public function getSymbol(): string
+    public function getName(): string
     {
+        return $this->name;
+    }
+
+    public function getCode(): string
+    {
+        return $this->iso_code;
+    }
+
+    public function getNumCode(): string
+    {
+        return $this->num_code;
+    }
+
+    public function getSymbol(int $index = 0): string
+    {
+        if ($this->display === self::DISPLAY_CODE) {
+            return $this->iso_code;
+        }
+
+        if (is_array($this->symbol)) {
+            if (!array_key_exists($index, $this->symbol)) {
+                throw new NoSuchCurrencySymbolException(
+                    __METHOD__,
+                    1,
+                    '$index',
+                    implode(',', [$index, count($this->symbol) - 1])
+                );
+            }
+
+            return $this->symbol[$index];
+        }
+
         return $this->symbol;
     }
 
@@ -73,13 +147,23 @@ class Currency
         return $this->position;
     }
 
-    public function setPosition(string $position): Currency
+    public function setPosition(int $position = self::POS_START): self
     {
-        if ($position !== self::POS_START || $position !== self::POS_END) {
+        if ($position !== self::POS_START && $position !== self::POS_END) {
             $position = self::POS_START;
         }
 
         $this->position = $position;
+        return $this;
+    }
+
+    public function setDisplay(int $display = self::DISPLAY_SYMBOL): self
+    {
+        if ($display !== self::DISPLAY_SYMBOL && $display !== self::DISPLAY_CODE) {
+            $display = self::DISPLAY_SYMBOL;
+        }
+
+        $this->display = $display;
         return $this;
     }
 }
