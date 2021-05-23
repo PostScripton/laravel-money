76129d29f86e31fdf6aa567ae776655953dab181
PostScription <68855126+PostScipton@users.noreply.github.com>
2021-05-23 21:17:43 +0300

    Added support of a local `.env`

--- src/MoneyServiceProvider.php
+++ src/MoneyServiceProvider.php
@@ -3,14 +3,17 @@
 namespace PostScripton\Money;
 
 use Illuminate\Support\ServiceProvider;
+use PostScripton\Money\Exceptions\ServiceClassDoesNotExistException;
+use PostScripton\Money\Exceptions\ServiceDoesNotHaveClassException;
+use PostScripton\Money\Exceptions\ServiceDoesNotInheritServiceException;
 use PostScripton\Money\Exceptions\UndefinedOriginException;
+use PostScripton\Money\Services\ServiceInterface;
 
 class MoneyServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->mergeConfigFrom($this->getConfigPath(), 'money');
-    }
+	public function boot()
+	{
+		$this->mergeConfigFrom($this->getConfigPath(), 'money');
 
     public function boot()
     {
@@ -27,21 +30,21 @@ class MoneyServiceProvider extends ServiceProvider
 				->setHasSpaceBetween(config('money.space_between', true))
 				->setOrigin(config('money.origin', MoneySettings::ORIGIN_INT));
 		} catch (UndefinedOriginException $e) {
-        	dd($e->getMessage());
+			dd($e->getMessage());
 		}
 
 		Money::set($settings);
-    }
+	}
 
-    protected function registerPublishing()
-    {
-        $this->publishes(
-            [
-                $this->getConfigPath() => config_path('money.php'),
-            ],
-            'money'
-        );
-    }
+	protected function registerPublishing()
+	{
+		$this->publishes(
+			[
+				$this->getConfigPath() => config_path('money.php'),
+			],
+			'money'
+		);
+	}
 
     private function getConfigPath(): string
     {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,20 @@
 
 namespace PostScripton\Money\Tests;
 
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use PostScripton\Money\MoneyServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+	protected function getEnvironmentSetUp($app)
+	{
+		// https://github.com/orchestral/testbench/issues/211#issuecomment-360885812
+
+		$app->useEnvironmentPath(__DIR__.'/..');
+		$app->bootstrapWith([LoadEnvironmentVariables::class]);
+		parent::getEnvironmentSetUp($app);
+	}
+
 	protected function getPackageProviders($app): array
 	{
 		return [
