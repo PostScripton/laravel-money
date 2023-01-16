3b85ea6e874e2e85e9bb3a68c66d40c373e266c4
PostScripton <postscripton.sp@gmail.com>
2023-01-16 23:25:14 +0300

    Extract tests

--- tests/Unit/MoneyTest.php
+++ tests/Unit/MoneyTest.php
@@ -3,9 +3,12 @@
 namespace PostScripton\Money\Tests\Unit;
 
 use InvalidArgumentException;
+use Mockery;
 use PostScripton\Money\Currency;
 use PostScripton\Money\Exceptions\MoneyHasDifferentCurrenciesException;
 use PostScripton\Money\Money;
+use PostScripton\Money\Services\ExchangeRateService;
+use PostScripton\Money\Services\ServiceInterface;
 use PostScripton\Money\Tests\TestCase;
 
 class MoneyTest extends TestCase
@@ -272,6 +275,97 @@ class MoneyTest extends TestCase
         $this->assertEquals('1500000', $m1->getAmount());
         $this->assertEquals('3000000', $m2->getAmount());
         $this->assertFalse($m1->equals($m2));
+    }
+
+    /** @dataProvider differenceDataProvider */
+    public function difference(string $first, string $second, string $result): void
+    {
+        $m1 = money_parse($first);
+        $m2 = money_parse($second);
+
+        $diff = $m1->difference($m2);
+
+        $this->assertInstanceOf(Money::class, $diff);
+        $this->assertTrue(money_parse($result)->equals($diff));
+    }
+
+    public function testDifferenceWithTwoDifferentCurrencies(): void
+    {
+        $this->app->bind(ServiceInterface::class, function () {
+            return Mockery::mock(ExchangeRateService::class)
+                ->makePartial()
+                ->shouldReceive('supports')
+                ->with(['USD', 'RUB'])
+                ->andReturn([])
+                ->shouldReceive('rate')
+                ->with('RUB', 'USD', null)
+                ->andReturn(1 / 65)
+                ->getMock();
+        });
+        $usd = money('500000');
+        $rub = money('1000000', currency('rub'));
+        $rubIntoUsd = $rub->convertInto($usd->getCurrency());
+        $expectedDifference = $usd->clone()->subtract($rubIntoUsd);
+
+        $difference = $usd->difference($rubIntoUsd);
+
+        $this->assertTrue($expectedDifference->equals($difference));
+    }
+
+    public function testExceptionIsThrownWhenThereAreTwoDifferentCurrencies(): void
+    {
+        $m1 = money('500000');
+        $m2 = money('1000000', currency('rub'));
+
+        $this->expectException(MoneyHasDifferentCurrenciesException::class);
+
+        $m1->difference($m2);
+    }
+
+    protected function differenceDataProvider(): array
+    {
+        return [
+            [
+                'first' => '$ 25',
+                'second' => '$ 100',
+                'result' => '$ 75',
+            ],
+            [
+                'first' => '$ 100',
+                'second' => '$ 25',
+                'result' => '$ 75',
+            ],
+            [
+                'first' => '$ 0',
+                'second' => '$ 25',
+                'result' => '$ 25',
+            ],
+            [
+                'first' => '$ 25',
+                'second' => '$ 0',
+                'result' => '$ 25',
+            ],
+            [
+                'first' => '$ 0',
+                'second' => '$ 0',
+                'result' => '$ 0',
+            ],
+            [
+                'first' => '$ 100',
+                'second' => '$ -25',
+                'result' => '$ 125',
+            ],
+            [
+                'first' => '$ -100',
+                'second' => '$ -115',
+                'result' => '$ 15',
+            ],
+            [
+                'first' => '$ -500',
+                'second' => '$ 100',
+                'result' => '$ 600',
+            ],
+        ];
     }
 
     protected function absoluteDataProvider(): array
